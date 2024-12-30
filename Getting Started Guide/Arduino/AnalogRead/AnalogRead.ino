1166951b1c8685a9bbb20eddcb6b3f85c963dbae
Muhammad Izzani Hanim <izzani@cytron.io>
2024-12-30 12:18:38 +0800

    Add Analog Read code for Robo ESP32

---
+++ Getting Started Guide/Arduino/AnalogRead/AnalogRead.ino
@@ -0,0 +1,46 @@
+/*
+DESCRIPTION:
+This example code will use Robo ESP32 to read the analog input from Maker Soil Module
+and then show the result on serial monitor. This code also applicable to any analog sensor.
+
+CONNECTION:
+
+-GROVE 1-      | Maker Soil
+Robo ESP32     | 
+---------------------------
+GND            | GND
+VCC            | VCC
+D39            | DIS
+D36            | OUT
+
+AUTHOR   : Cytron Technologies Sdn Bhd
+WEBSITE  : www.cytron.io
+EMAIL    : support@cytron.io
+*/
+
+const int MakerSoil = 39;
+int inputvalue = 0;
+float readingvalue = 0;
+
+void setup() {
+
+  pinMode(MakerSoil, INPUT);
+  Serial.begin(115200);
+}
+
+
+void loop() {
+
+  inputvalue = analogRead(MakerSoil);
+
+  readingvalue = (inputvalue * 5.0 / 4096);
+
+  Serial.print("Input Value : ");
+  Serial.println(inputvalue);
+  Serial.print("Reading Value : ");
+  Serial.println(readingvalue);
+  Serial.println("---------------------------");
+  
+  delay(1000);
+
+}
